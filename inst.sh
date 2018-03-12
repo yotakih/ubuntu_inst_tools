56b6f003ba8d0a069d45db020ff755f8b779cc5c
yotakih <37173608+yotakih@users.noreply.github.com>
2018-03-13 06:42:22 +0900

    use apt commands

--- inst.sh
+++ inst.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 #users directory 
-LANG=C xdg-user-dirs-gtk-update
+#LANG=C xdg-user-dirs-gtk-update
 
 #strace
-apt-get install strace
+apt install strace
 #ltrace
-apt-get install ltrace
+apt install ltrace
 #radare2
 mkdir ~/radare2/
 git clone https://github.com/radare/radare2 
 cd radare2
 sys/install.sh
 #ghex
-apt-get install ghex
+apt install ghex
 #peda
 git clone https://github.com/zachriggle/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
@@ -24,19 +24,21 @@ wget https://github.com/slimm609/checksec.sh/archive/1.6.tar.gz
 tar zxvf 1.6.tar.gz
 cp checksec.sh-1.6/checksec $HOME/bin/checksec.sh
 #pbcopy
-apt-get install xsel
+apt install xsel
 echo "alias pbcopy='xsel --clipboard --input'">>~/.bashrc
 #rp
 wget https://github.com/downloads/0vercl0k/rp/rp-lin-x64 -O $HOME/bin/rp
 #nodejs
-apt-get install nodejs
+apt install nodejs
 #npm
-apt-get install npm
+apt install npm
 #n
 npm -g install n
 #node lst version install
 n lts
 #socat
-apt-get install socat
+apt install socat
 #volatility
-apt-get install volatility
+apt install volatility
+#sqlite3
+apt install sqlite3
